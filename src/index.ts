ecfe20feff2704156feafdde5e17bce19f69af30
lmmontejo85926 <lmmontejo85926@liceo.edu.ph>
2026-03-27 13:08:38 +0800

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Server is working 🚀");
+});
 
 serve({
   fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+  port: 3000,
+});
+
+console.log("Server running on http://localhost:3000");
